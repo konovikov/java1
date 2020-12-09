8fefdf8ff59755e23a224f9f87962ee648a3e811
Konstantin <konovikov@yandex.ru>
2020-12-09 19:55:22 +0300

    HelloWorld start

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,20 +2,14 @@ package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
 
-    public static final double R = 6371.2;
-    public static final double PI = 3.14;
-
-    //Формула объёма шара 4/3πR3
-
     public static double volumeBallDouble(double radius){
         double v1 = 4.0 / 3.0 * 3.14 * radius * radius * radius;
         return v1;
     }
 
-
     public static float volumeBallFloat(float radius){
-        float v2 = (float)(4.0 / 3.0 * 3.14 * radius * radius * radius);
-        return (float)v2;
+        float v2 = (float)(4.0 / 3.0 * 3.14F * radius * radius * radius);
+        return v2;
     }
 
     public static double calculateAccuracy(double radius) {
