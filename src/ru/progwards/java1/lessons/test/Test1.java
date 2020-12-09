b0dcf42de70b3ce3dbc98cba02f511fcc2798e02
Konstantin <konovikov@yandex.ru>
2020-12-09 14:07:25 +0300

    HelloWorld start

---
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -0,0 +1,7 @@
+package ru.progwards.java1.lessons.test;
+
+public class Test1 {
+    public static void main(String[] args){
+        System.out.print("делаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+    }
+}
